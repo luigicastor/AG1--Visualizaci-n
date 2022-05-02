a17dcc9742f22a5a0cc9c4a46abfe6b69f45c2ca
luigicastor <luicastor86@gmail.com>
2022-05-02 21:17:13 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-import * as d3 from 'd3plus';
+
 
 
 //Gráfico de columnas. Mi 1º gráfico
@@ -15,6 +15,19 @@ var visualization = d3plus.viz()
 .y("value")
 .draw()
 
+d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
+
+  var visualization = d3plus.viz()
+  .container("#viz2")
+  .data(data)
+  .type('bar')
+  .id('name')
+  .x('year')
+  .y('value')
+  .axes({ ticks: 'false' })
+  .draw();
+  
+  });
 
 var data = [
   {"year": 1991, "name":"alpha", "value": 15},
